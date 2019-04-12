026356d5320dd657e1aa358fed0c529643e49fd9
Leon V <leon99@outlook.com>
2019-04-12 16:04:10 +1000

    Enable running w/o certificate
    SSL termination should be done outside

--- src/Monyk.GroundControl.Main/Startup.cs
+++ src/Monyk.GroundControl.Main/Startup.cs
@@ -71,11 +71,6 @@ namespace Monyk.GroundControl.Main
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             //app.UseHttpsRedirection();
             app.UseMvc();
