622ae253f77c2fe633326f37a7c789af90ff47c3
Leon V <leon99@outlook.com>
2019-04-17 19:41:34 +1000

    Improve diagnostics

--- src/Monyk.GroundControl.Main/Services/Launcher.cs
+++ src/Monyk.GroundControl.Main/Services/Launcher.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Monyk.GroundControl.Db;
 using Monyk.GroundControl.Services;
 
@@ -11,15 +12,18 @@ namespace Monyk.GroundControl.Main.Services
     {
         private readonly GroundControlDbContext _db;
         private readonly MonitorScheduler _scheduler;
+        private readonly Logger<Launcher> _logger;
 
-        public Launcher(GroundControlDbContext db, MonitorScheduler scheduler)
+        public Launcher(GroundControlDbContext db, MonitorScheduler scheduler, Logger<Launcher> logger)
         {
             _db = db;
             _scheduler = scheduler;
+            _logger = logger;
         }
 
         public void Prepare(IHostingEnvironment env)
         {
+            _logger.LogInformation("Preparing Ground Control");
             Directory.SetCurrentDirectory(env.ContentRootPath); // To make it consistent across different hosts
 
             if (env.IsDevelopment())
@@ -34,7 +38,9 @@ namespace Monyk.GroundControl.Main.Services
 
         public void StartMonitoring()
         {
-            foreach (var monitor in _db.Monitors.Where(m => !m.IsDeleted && !m.IsStopped))
+            var monitors = _db.Monitors.Where(m => !m.IsDeleted && !m.IsStopped);
+            _logger.LogInformation($"Initial scheduling for {monitors.Count()} monitors");
+            foreach (var monitor in monitors)
             {
                 _scheduler.AddSchedule(monitor);
             }
